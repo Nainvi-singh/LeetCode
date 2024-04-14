40960014c42208acc0f2255d2a3fb2d540faf7d0
Nainvi-singh <98397525+Nainvi-singh@users.noreply.github.com>
2024-04-14 21:09:53 +0530

    Time: 5 ms (38.76%), Space: 42.4 MB (17.17%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        //List<String> smallList=new ArrayList<>();
+        String smallList="";
+        List<String> FinalList=new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return new ArrayList();
+        }
+        int index=0;
+        String map[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        helper(index,digits,smallList,FinalList,map);
+        return FinalList;
+        
+        
+    }
+    
+    void helper(int index,String digits, String smallList, List<String> FinalList,String map[]){
+        
+        //base case
+         if(index>=digits.length()){
+            FinalList.add(smallList);
+             return ;
+             
+        }
+
+
+        int number=digits.charAt(index)-  '0';
+        String value=map[number];
+         
+        //iterate the num
+         for(int i=0;i<value.length(); i++){           
+                smallList=smallList+(value.charAt(i));//modify the value
+                helper(index+1,digits,smallList,FinalList,map);//recursive call
+                    //stack fall
+                 smallList=smallList.substring(0, smallList.length() - 1);  
+                
+               
+        }
+    }
+}
