b8b71e4e12c2546b64d0ab7131e81e31ee7039ee
Nainvi-singh <98397525+Nainvi-singh@users.noreply.github.com>
2024-06-05 11:07:40 +0530

    Time: 11 ms (35.61%), Space: 45.1 MB (7.56%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> commonChars(String[] words) {
+         List<String> result = new ArrayList<>();
+        
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            int minCount = Integer.MAX_VALUE;
+
+            for (String word : words) {
+                int count = 0;
+                for (char c : word.toCharArray()) {
+                    if (c == ch) {
+                        count++;
+                    }
+                }
+                minCount = Math.min(minCount, count);
+            }
+            for (int i = 0; i < minCount; i++) {
+                String s=Character.toString(ch);
+                result.add(s);
+            }
+        }
+        return result;
+    }
+}
