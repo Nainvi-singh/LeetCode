ed1445017cf3cf4975b4f2322c86ba13473ca389
Nainvi-singh <98397525+Nainvi-singh@users.noreply.github.com>
2024-06-11 16:42:27 +0530

    Time: 2 ms (100.00%), Space: 57.7 MB (84.70%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -1,20 +1,19 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        //creating prefix array
-        long prefix[]=new long[nums.length];
-        prefix[0]=nums[0];
-        //fill rhe prefix element
-        for(int i=1;i<nums.length;i++){
-            prefix[i]=prefix[i-1]+nums[i];
+        //apporach 2
+        long entireSum=0;
+        int count=0;
+        for(int i:nums){
+            entireSum=entireSum+i;
         }
-        int countValidSplit=0;
+        long leftSum=0;
         for(int i=0;i<nums.length-1;i++){
-            long leftpart=prefix[i];
-            long rightpart=prefix[prefix.length-1]-prefix[i];
-                if(leftpart>=rightpart){
-                    countValidSplit++;
-                }       
+            leftSum=leftSum+nums[i];
+            entireSum=entireSum-nums[i];
+            if(leftSum>=entireSum){
+                count++;
+            }
         }
-        return countValidSplit;
+        return count;
     }
 }
