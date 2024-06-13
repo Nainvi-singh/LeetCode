00950c10f60849ed4c452a1deada5582fa37fa20
Nainvi-singh <98397525+Nainvi-singh@users.noreply.github.com>
2024-06-13 15:54:36 +0530

    Time: 13 ms (30.41%), Space: 49.3 MB (79.33%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+            //apporach 1 brute force
+            //2 loop
+        
+            //apporach 2
+            //hashing
+            HashMap<Integer,Integer> map=new HashMap<>();
+            for(int val:nums){
+                Integer count=map.get(val);
+                if(count!=null){
+                    count++;;
+                    map.put(val,count);
+                    
+                }
+                else{
+                    count=1;
+                    map.put(val,count);
+                }
+                if(count>nums.length/2){
+                    return val;
+                }
+                
+            }
+            return 0;
+    }
+}
