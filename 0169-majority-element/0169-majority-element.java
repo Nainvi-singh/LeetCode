4517047ece427efbb35dc41928353fe238e36d3c
Nainvi-singh <98397525+Nainvi-singh@users.noreply.github.com>
2024-06-13 16:04:54 +0530

    Time: 3 ms (61.76%), Space: 53.1 MB (30.26%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -5,23 +5,27 @@ class Solution {
         
             //apporach 2
             //hashing
-            HashMap<Integer,Integer> map=new HashMap<>();
-            for(int val:nums){
-                Integer count=map.get(val);
-                if(count!=null){
-                    count++;;
-                    map.put(val,count);
+//             HashMap<Integer,Integer> map=new HashMap<>();
+//             for(int val:nums){
+//                 Integer count=map.get(val);
+//                 if(count!=null){
+//                     count++;;
+//                     map.put(val,count);
                     
-                }
-                else{
-                    count=1;
-                    map.put(val,count);
-                }
-                if(count>nums.length/2){
-                    return val;
-                }
+//                 }
+//                 else{
+//                     count=1;
+//                     map.put(val,count);
+//                 }
+//                 if(count>nums.length/2){
+//                     return val;
+//                 }
                 
-            }
-            return 0;
+//             }
+//             return 0;
+        
+        //apporach 3-sorting
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 }
