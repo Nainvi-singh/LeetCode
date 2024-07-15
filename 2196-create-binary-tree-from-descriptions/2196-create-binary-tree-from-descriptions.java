cf03f42a7d6e83ce7b35bad96edbee7deea48d05
Nainvi-singh <98397525+Nainvi-singh@users.noreply.github.com>
2024-07-15 09:25:58 +0530

    Time: 13 ms (97.68%), Space: 55.1 MB (86.75%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.java
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.java
@@ -18,10 +18,16 @@ class Solution {
         TreeNode[] map = new TreeNode[100001];
         boolean[] child = new boolean[100001];
         for (int[] d : descriptions){
-            if (map[d[0]] == null) map[d[0]] = new TreeNode(d[0]);
+            if (map[d[0]] == null) {
+                map[d[0]] = new TreeNode(d[0]);
+            }
             TreeNode node = (map[d[1]] == null ? new TreeNode(d[1]) : map[d[1]]);
-            if (d[2] == 1) map[d[0]].left = node;
-            else map[d[0]].right = node;
+            if (d[2] == 1) {
+                map[d[0]].left = node;
+            }
+            else {
+                map[d[0]].right = node;
+            }
             map[node.val] = node;
             child[d[1]] = true;
         }
