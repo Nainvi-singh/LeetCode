f787682b42cfb88061337b5650800a32d6e7bb14
Nainvi-singh <98397525+Nainvi-singh@users.noreply.github.com>
2024-07-19 17:03:25 +0530

    Time: 1 ms (91.63%), Space: 47.9 MB (22.28%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,24 +1,52 @@
 class Solution {
-    void helper(int node,int[][] isConnected,boolean visited[]){
+    // void helper(int node,int[][] isConnected,boolean visited[]){
+    //     visited[node]=true;
+    //     for(int i=0;i<isConnected.length;i++){
+    //         if(isConnected[node][i]==1 && !visited[i]){  //connection is there
+    //             helper(i,isConnected,visited);
+    //         }
+    //     }
+    // }
+    // public int findCircleNum(int[][] isConnected) {
+    //     int countComponent=0;
+    //     boolean visited[]=new boolean[isConnected.length];
+    //     for(int i=0;i<isConnected.length;i++){
+    //         //if vertex not visited
+    //         if(!visited[i]){
+    //             countComponent++;
+    //             helper(i,isConnected,visited);
+    //         }
+    //     }
+    //     return countComponent;
+    // }
+    
+    void bfsHelper(int node,int[][] isConnected,boolean visited[]){
+        Queue<Integer> queue=new LinkedList<>();
+        queue.offer(node);
         visited[node]=true;
-        for(int i=0;i<isConnected.length;i++){
-            if(isConnected[node][i]==1 && !visited[i]){  //connection is there
-                helper(i,isConnected,visited);
+        while(!queue.isEmpty()){
+            int currentNode=queue.poll();
+            for(int i=0;i<isConnected.length;i++){
+                if(isConnected[currentNode][i]==1 && !visited[i]){
+                    queue.offer(i);
+                    visited[i]=true;
+                }
             }
         }
     }
+    
     public int findCircleNum(int[][] isConnected) {
         int countComponent=0;
         boolean visited[]=new boolean[isConnected.length];
         for(int i=0;i<isConnected.length;i++){
-            //if vertex not visited
             if(!visited[i]){
                 countComponent++;
-                helper(i,isConnected,visited);
+                bfsHelper(i,isConnected,visited);
             }
         }
         return countComponent;
     }
+    
 }
 //connected component
 
