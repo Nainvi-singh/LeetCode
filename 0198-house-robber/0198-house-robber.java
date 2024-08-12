4f1132e8ae651c359c7089aa1d4c190a87834676
Nainvi-singh <98397525+Nainvi-singh@users.noreply.github.com>
2024-08-12 08:22:45 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (56.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,5 +1,17 @@
 class Solution {
-    private int[] cache; //memoization
+    
+    /*
+        //recurance 
+        int result=Math.max(robInHouse(i+1,money),robInHouse(i+2,money)+money[i]);
+        dp[i]=max(dp[i-1],dp[i-2]+nums[i])
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+    */
+        
+    
+    //memoization
+    /*
+    private int[] cache; 
     public int rob(int[] nums) {
         cache=new int[100];
         Arrays.fill(cache,-1);
@@ -16,5 +28,24 @@ class Solution {
         int result=Math.max(robInHouse(i+1,money),robInHouse(i+2,money)+money[i]);
         cache[i]=result;
         return result;
+    }
+    */
+    
+    //Bottom up
+    public int rob(int[] nums) {
+        
+        if(nums.length==0){
+            return 0;
+        }
+        if(nums.length==1){
+            return nums[0];
+        }
+        int dp[]=new int[nums.length];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[nums.length-1];//last has the max
     }
 }
