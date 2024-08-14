8d5f6a25e4475acbde4d7d35671f9a8c1f425094
Nainvi-singh <98397525+Nainvi-singh@users.noreply.github.com>
2024-08-14 20:42:44 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (10.77%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -13,19 +13,35 @@
  *     }
  * }
  */
+class Pair{
+    TreeNode p;
+    TreeNode q;
+    Pair(TreeNode p,TreeNode q){
+        this.p=p;
+        this.q=q;
+    }
+    
+}
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null&& q==null){
-            return true;
+        Stack<Pair> stack=new Stack<>();
+        stack.push(new Pair(p,q));
+        while(!stack.isEmpty()){
+            Pair pair=stack.pop();
+            TreeNode node1=pair.p;
+            TreeNode node2=pair.q;
+            if(node1==null &&node2==null){
+                continue;
+            }
+             if(node1==null || node2==null){
+                return false;
+            }
+            if(node1.val!=node2.val){
+                return false;
+            }
+            stack.push(new Pair(node1.left,node2.left));
+            stack.push(new Pair(node1.right,node2.right));
         }
-        if(p==null || q==null){
-            return false;
-        }
-        if(p.val!=q.val){
-            return false;
-        }
-        boolean left=isSameTree(p.left,q.left);
-        boolean right=isSameTree(p.right,q.right);
-        return left&&right;
+        return true;
     }
 }
