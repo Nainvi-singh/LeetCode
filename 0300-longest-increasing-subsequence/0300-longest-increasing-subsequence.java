069449152e9baa0a1f514b1491053f99cc5b4f6d
Nainvi-singh <98397525+Nainvi-singh@users.noreply.github.com>
2024-08-20 21:27:15 +0530

    Time: 51 ms (35.30%), Space: 43.8 MB (93.25%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,23 +1,22 @@
 class Solution {
+    //bottom up
     Map<Integer,Integer> cache=new HashMap<>();
+    
     public int lengthOfLIS(int[] nums) {
-        int result=0;
-        for(int i=0;i<nums.length;i++){
-            result=Math.max(result, helper(nums,i));
-        }
-        return result;
-    }
-    int helper(int[] nums,int i){
-        if(cache.containsKey(i)){
-            return cache.get(i);
-        }
+        int len=nums.length;
+        int dp[]=new int [len];
+        Arrays.fill(dp,1);
         int result=1;
-        for(int j=0;j<i;j++){
-            if(nums[i]>nums[j]){
-                result=Math.max(result, helper(nums,j)+1);
+        for(int i=0;i<len;i++){
+            for(int j=0;j<i;j++){        
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                    result=Math.max(result,dp[i]);
+                }
+                //result=Math.max(result, helper(nums,j)+1);  //this is reccurence relation
+                
             }
         }
-        cache.put(i,result);
         return result;
     }
 }
