dcf3ceba4527c01c80bf2db8162be38f5a3a140d
Nainvi-singh <98397525+Nainvi-singh@users.noreply.github.com>
2024-08-21 20:09:22 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (29.26%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -3,7 +3,8 @@ class Solution {
         int writer=0;
         for(int reader=0;reader<nums.length;reader++){
             if(writer<2||nums[reader]>nums[writer-2]){
-                nums[writer++]=nums[reader];
+                nums[writer]=nums[reader];
+                writer++;
             }
         }
         return writer;
